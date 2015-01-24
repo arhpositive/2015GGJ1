71e6cdc4a20d2b4201fba0d7c2dce41d3e59d20e
arhpositive <arhpositive@gmail.com>
2015-01-24 15:43:45 +0200

    ball behaviour changes;

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -10,47 +10,45 @@ public class BallBehaviour : MonoBehaviour {
     bool gameOn;
 
 	void OnTriggerExit(Collider other) {
-		if (other.tag == "sceneBounds") {
-            
-            
-
-			transform.position = initial_pos;
-			rigidbody.velocity = Vector3.zero;
+		if (other.tag == "sceneBounds") {			
 			reset ();
 		}
 	}
     
 	void Start () {
+
 		initial_pos = transform.position;
 		camera = GameObject.Find ("Main Camera");
-        	leftGuy = GameObject.FindGameObjectWithTag("leftGuy");
-        	rightGuy = GameObject.FindGameObjectWithTag("rightGuy");
+        leftGuy = GameObject.FindGameObjectWithTag("leftGuy");
+        rightGuy = GameObject.FindGameObjectWithTag("rightGuy");
 
-        	// Switch to UI Camera, initiate game mode as off
-        	myCamera = GameObject.FindWithTag("MainCamera");
-        	uiCamera = GameObject.FindWithTag("UICamera");
+        // Switch to UI Camera, initiate game mode as off
+        myCamera = GameObject.FindWithTag("MainCamera");
+        uiCamera = GameObject.FindWithTag("UICamera");
 		reset ();
 	}
 
 	public void reset(){
-	myCamera.transform.rotation = Quaternion.LookRotation (-Vector3.up, Vector3.forward);
+        ResetBall();
+	    myCamera.transform.rotation = Quaternion.LookRotation (-Vector3.up, Vector3.forward);
 
         // Switch to UI Camera, game mode off
         //rigidbody.isKinematic = true;
         rigidbody.useGravity = false;
         gameOn = false;
         GameObject playButton = GameObject.FindWithTag("UI_PlayButton");
-//        playButton.guiText.text = "Replay";
+        //playButton.guiText.text = "Replay";
         myCamera.SetActive(false);
         uiCamera.SetActive(true);
 	}
 
     public void InitiateBouncing()
     {
+        ResetBall();
         uiCamera.SetActive(false);
         myCamera.SetActive(true);
         gameOn = true;
-//        rigidbody.isKinematic = false;
+//      rigidbody.isKinematic = false;
         rigidbody.useGravity = true;
     }
 
@@ -75,8 +73,30 @@ public class BallBehaviour : MonoBehaviour {
             myCamera.transform.position = transform.position + myCamera.transform.rotation * Vector3.forward * -3.5f + Vector3.up * 1.5f;
         }
         else {
-//            rigidbody.isKinematic = true;
+//          rigidbody.isKinematic = true;
             rigidbody.useGravity = false;
         }
 	}
+
+    void ResetBall()
+    {
+        gameObject.layer = 8; //reset to leftGuy;
+
+        foreach (Transform tr in leftGuy.transform)
+        {
+
+            if (tr.gameObject.CompareTag("rightArm") == true)
+            {
+                //set position of ball slightly above arm
+                BoxCollider boxCollider = (BoxCollider)tr.gameObject.collider;
+
+                
+
+                Vector3 colliderWorldPos = tr.TransformPoint(boxCollider.center);
+
+                transform.position = colliderWorldPos + Vector3.up * 5.0f;
+            }
+        }
+        rigidbody.velocity = Vector3.zero;
+    }
 }
