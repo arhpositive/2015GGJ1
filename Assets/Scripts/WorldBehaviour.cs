c9ac407300c6d95d42ac87c138a782d3cf028eda
Engin <shultays@gmail.com>
2015-01-24 23:38:57 +0200

    added new world model

--- Assets/Scripts/WorldBehaviour.cs
+++ Assets/Scripts/WorldBehaviour.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class WorldBehaviour : MonoBehaviour {
 
 
+	void Start(){
 
+	}
 
 	void Update () {
-		this.transform.rotation = this.transform.rotation * Quaternion.AngleAxis(Time.deltaTime, Vector3.left);
+		this.transform.rotation = this.transform.rotation * Quaternion.AngleAxis(-Time.deltaTime*10.0f, Vector3.left);
 	}
 }
