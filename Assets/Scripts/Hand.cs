9e5fa65ff7d393acfb62e4c6f4a6aa1c728e5ac2
arhpositive <arhpositive@gmail.com>
2015-01-24 12:50:23 +0200

    prefab fixes

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 public class Hand : MonoBehaviour {
-    BoxCollider collider;
+    BoxCollider boxCollider;
     public float distortFactor;
+    public static float zDistFromCenter = 26.0f;
 
 	// Use this for initialization
 	void Start () {
-        collider = GetComponent<BoxCollider>();
+        boxCollider = GetComponent<BoxCollider>();
 	}
 	
 	// Update is called once per frame
@@ -22,27 +23,25 @@ public class Hand : MonoBehaviour {
 
         // Compute destination point.
         ContactPoint contact = collision.contacts[0];
-        /*
-         * Logic below assumes edges of the box collider is parallel to axes.
-         */
-        Vector3 colliderSize = Vector3.Scale(transform.localScale, collider.size);
+
+        Vector3 colliderSize = Vector3.Scale(transform.localScale, boxCollider.size);
         Vector3 colliderTopCenter;
-        colliderTopCenter = transform.position + collider.center;
+        colliderTopCenter = transform.position + boxCollider.center;
         colliderTopCenter.y = colliderSize.y / 2;
 
         float distFromHandCenter = (contact.point.x - colliderTopCenter.x);
-        float zDistFromCenter = 26f;  // TODO: Retrieve this from the base size & position.
         float finalX = contact.point.x + distFromHandCenter * distortFactor;
         float finalZ = (contact.point.z > 0 ? -zDistFromCenter : zDistFromCenter);
         Vector3 destPoint = new Vector3(finalX, body.position.y, finalZ);
 
         // Compute velocity vector.
-        float vox, voy, voz, duration = 2.0f; // TODO: dynamically alter duration based on slider action.
+        float duration = 2.0f; // TODO: dynamically alter duration based on slider action.
+        float vox, voy, voz; 
         vox = (destPoint.x - body.position.x) / duration;
         voz = (destPoint.z - body.position.z) / duration;
         voy = (0.5f * duration * duration * Physics.gravity.magnitude) / duration;
-        Vector3 pushDir = new Vector3(vox, voy, voz);
 
+        Vector3 pushDir = new Vector3(vox, voy, voz);
         body.velocity = pushDir;
     }
 }
