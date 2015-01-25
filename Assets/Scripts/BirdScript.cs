cf0d0911768aaea4b5841be62c5a74e7c5502be7
arhpositive <arhpositive@gmail.com>
2015-01-25 14:55:11 +0200

    bird flocks & dogs

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.left * Time.deltaTime * 5.0f);
+	}
+}
